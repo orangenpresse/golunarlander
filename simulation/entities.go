06fcb88bb7e56e9dacedc8fdcfd58771db7f39e2
User <user@VM-Ubuntu-14.04-x64>
2014-10-04 03:51:08 +0200

    sdf

---
+++ simulation/entities.go
@@ -0,0 +1,19 @@
+package simulation
+
+type Vector2D struct {
+	X float64
+	Y float64
+}
+
+type LanderState struct {
+	Exploded bool
+	Fuel     int // 100% - 0%
+}
+
+type Lander struct {
+	position       Vector2D
+	velocity       Vector2D
+	thrust         float64
+	crashTolerance float64
+	state          LanderState
+}
