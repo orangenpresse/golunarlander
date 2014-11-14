b6ee9f25a4d07cb7801eec085bb7dedde02562ec
Benedict Burckhart <bb@stilgrenze.de>
2014-11-14 19:18:41 +0100

    3d shader

--- game/shader/vertexShader330.glsl
+++ game/shader/vertexShader330.glsl
@@ -4,8 +4,8 @@ uniform mat4 projection;
 uniform mat4 camera;
 uniform mat4 model;
 
-in vec2 position;
+in vec3 position;
 
 void main() {
-    gl_Position = projection * camera * model * vec4(position, 0.0, 1.0);
+    gl_Position = projection * camera * model * vec4(position, 1.0);
 }
